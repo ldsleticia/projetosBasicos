4a24c46c1649130eb3e69f638b6f0cb2c2ebaef6
ldsleticia <lds.leticia.dos.santos@gmail.com>
2021-02-23 02:01:43 -0300

    novos projetos

---
+++ funcoes/script.js
@@ -0,0 +1,19 @@
+/* aplicativo de frases motivacionais */
+
+/* declaração da função - function statement */
+function criarFrases(){
+	console.log("Estudar é muito bom");
+	console.log("Paciência e persistência");
+	console.log("Revisão é mãe do aprendizado");
+}
+
+/* executar a função; chamar a função, call function */
+criarFrases();
+
+/* função anônima ou function expression */
+/* 										parâmetros				 */
+const sum = function(num1, num2){
+	console.log(num1 + num2);
+}
+
+sum(2, 3); /* argumentos */
